ee54cc491818497072fa34d390b6d776a86aa6ea
AIT OUHAMOU SAMY ISLEM <ms_aitouhamou@esi.dz>
2025-01-02 13:29:55 +0100

    v1.1

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { PatientListComponent } from './components/patient-list/patient-list.component'; // Ensure path is correct
+import { SearchHeaderComponent } from './components/Header-dashboard/SearchHeader.component'; // Ensure path is correct
+import {DashboardHeaderComponent} from './components/Header-dashboard/header.component';
+import {SidebarComponent} from './components/left-bar/left-bar.component';
+import {TestResultsComponent} from './components/stats/stats.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  standalone: true,
+  imports: [CommonModule, PatientListComponent, SearchHeaderComponent, DashboardHeaderComponent, SidebarComponent, TestResultsComponent]
 })
 export class AppComponent {
-  title = 'frontend';
+  patients = [
+    { id: 1, name: { first: 'John', last: 'Doe' }, assignedDoctor: 'Dr. Smith', date: '2024-01-01', disease: 'Flu', nss: '12345', birthDate: '', phoneNumber: '', address: '' },
+    { id: 2, name: { first: 'Jane', last: 'Doe' }, assignedDoctor: 'Dr. Lee', date: '2024-01-02', disease: 'Cold', nss: '67890', birthDate: '', phoneNumber: '', address: '' }
+  ];
+  chartData = [
+    { label: 'Cholestérol', current: 200, previous: 300 },
+    { label: 'Fer', current: 120 },
+    { label: 'Hypertension', current: 180, previous: 150 },
+  ];
+
 }
